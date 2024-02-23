938de295151252af93783b79d8d350333a9ac339
patttttyyyyy <patriciaannsampang8@gmail.com>
2024-02-23 15:49:42 +0800

    initial push

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Leaf_Year-Sampang,Patricia-Ann"
+include(":app")
+ 
